7d6c9102ec86e2294b5c36a40f41e35dd1a7fcc2
TiagoFueber <tfuelber@gmail.com>
2019-04-24 21:26:16 -0300

    Add Modal Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { Route, HashRouter } from 'react-router-dom';
-import { Normalize } from 'styled-normalize';
-import GlobalStyle from './view/ui/styles/GlobalStyle';
-import StartPage from './view/start-page/StartPage';
-import QuizPage from './view/quiz/QuizPage';
-import store from './state/store';
+import React from "react";
+import { Provider } from "react-redux";
+import { Route, HashRouter } from "react-router-dom";
+import { Normalize } from "styled-normalize";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import GlobalStyle from "./view/ui/styles/GlobalStyle";
+import StartPage from "./view/start-page/StartPage";
+import QuizPage from "./view/quiz/QuizPage";
+import store from "./state/store";
+
+library.add(faTimes);
 
 const App = () => (
   <Provider store={store}>
